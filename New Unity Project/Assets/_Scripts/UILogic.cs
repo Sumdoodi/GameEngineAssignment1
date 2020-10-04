45af607d520a1a096e6ccaf850ff878739ffdf7a
Steph <stephane.gagnon00@gmail.com>
2020-10-04 11:56:44 -0400

    Start interactable UI, update proper gitignore
    used gitignore from GitHub made for Unity projects

---
+++ New Unity Project/Assets/_Scripts/UILogic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILogic : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject canvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnCube()
+    {
+        Debug.Log("Cube");
+    }
+
+    public void SpawnSphere()
+    {
+        Debug.Log("Sphere");
+    }
+}
