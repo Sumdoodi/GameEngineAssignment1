dea3448f4b3bb523259b88655b68639c3c4e862c
Steph <stephane.gagnon00@gmail.com>
2020-10-04 15:13:41 -0400

    spawn where looking

--- New Unity Project/Assets/_Scripts/UILogic.cs
+++ New Unity Project/Assets/_Scripts/UILogic.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class UILogic : MonoBehaviour
 {
+    // references
     [SerializeField]
     public GameObject canvas;
+    [SerializeField]
+    public Camera player;
+
+    // prefabs
+    [SerializeField]
+    public GameObject cubePrefab;
+    [SerializeField]
+    public GameObject spherePrefab;
+
+    // vars
+    public float placementDistance = 1.0f;
 
     // Update is called once per frame
     void Update()
@@ -15,11 +27,27 @@ public class UILogic : MonoBehaviour
 
     public void SpawnCube()
     {
+        Instantiate<GameObject>(cubePrefab, GetSpawnLocation(), GetSpawnRotation());
         Debug.Log("Cube");
     }
 
     public void SpawnSphere()
     {
+        Instantiate<GameObject>(spherePrefab, GetSpawnLocation(), GetSpawnRotation());
         Debug.Log("Sphere");
+    }
+
+    public Vector3 GetSpawnLocation()
+    {
+        Vector3 basePos = player.transform.position;
+        Vector3 placePos = basePos + (player.transform.forward * placementDistance);
+
+        return placePos;
+    }
+
+    public Quaternion GetSpawnRotation()
+    {
+
+        return new Quaternion();
     }
 }
