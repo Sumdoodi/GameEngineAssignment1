0021f113e19ac232115d83c0dc7b424ba6c09054
Jonah Griffin <jonah.griffin@uoit.net>
2020-10-04 16:17:32 -0400

    Shift/Space controls

--- New Unity Project/Assets/_Scripts/Movement.cs
+++ New Unity Project/Assets/_Scripts/Movement.cs
@@ -9,6 +9,7 @@ public class Movement : MonoBehaviour
     public float mouseY;
     public float posX;
     public float posZ;
+    public float posY;
     public float mouseSensitivity = 500;
     public float speed = 15.0f;
 
@@ -48,11 +49,34 @@ public class Movement : MonoBehaviour
 
             //transform.Rotate(Vector3.up * mouseY);
 
+            //Directional Movement
             posX = Input.GetAxis("Horizontal");
             posZ = Input.GetAxis("Vertical");
 
             Vector3 move = transform.right * posX + transform.forward * posZ;
 
+            Vector3 yMove = move;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                posY = 1;
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                posY = -1;
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                posY = 0;
+            }
+            else if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                posY = 0;
+            }
+
+            yMove = transform.up * posY;
+
+            move = new Vector3(move.x, yMove.y, move.z);
             transform.position += move * speed * Time.deltaTime;
         }
     }
