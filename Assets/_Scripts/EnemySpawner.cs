61c6f3daa6330babb2708292686e2a89faa116df
Dhruva <dhruva.guruprasad@gmail.com>
2020-10-20 23:04:14 -0400

    factory design pattern implemented (random enemy spawner)

---
+++ Assets/_Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private float spawnRatePerMinute = 60;
+
+    private int currentCount = 0;
+
+    [SerializeField]
+    private TimedObjectFactory factory;
+
+    private void Update()
+    {
+        var targetCount = Time.time * (spawnRatePerMinute / 60);
+
+        while (targetCount > currentCount)
+        {
+            var inst = factory.GetNewInstance();
+            inst.transform.position = new Vector3(Random.Range(-7.0f, 7.0f), Random.Range(-5.0f, 5.0f), Random.Range(-10.0f, 10.0f));
+
+            currentCount++;
+        }
+    }
+}
+
